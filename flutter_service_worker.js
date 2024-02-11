a3f3e9f6088d81e62521dc245c842a645a647555
hari <hari@live.in>
2024-02-11 23:52:17 +0530

    material design kit ver 1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -128,8 +128,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "dcd59431cdfdbaad3550f58d15c5dea6
 "icons/Icon-512.png": "cfa6b4faa41b623f826086437eed92d7",
 "icons/Icon-maskable-192.png": "cfa6b4faa41b623f826086437eed92d7",
 "icons/Icon-maskable-512.png": "cfa6b4faa41b623f826086437eed92d7",
-"index.html": "6b0490eafa42b5d2f5f5de2223d6ba87",
-"/": "6b0490eafa42b5d2f5f5de2223d6ba87",
+"index.html": "48f3e0ffc462fc7d5aa34c98479a2fa5",
+"/": "48f3e0ffc462fc7d5aa34c98479a2fa5",
 "main.dart.js": "a951029ccb4340a8c8f42d860261d8fb",
 "manifest.json": "d5d6187051e77f9bb253177a6ca4632b",
 "version.json": "1df16c850b5a94499d62b5909a2e1e65"};
